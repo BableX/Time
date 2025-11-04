0625bb1ff1fa1d66bc89412764f084e1cb8357ef
MARSEL ATTOKUROV <blackdeyzir@gmail.com>
2025-11-04 22:12:40 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let clock = () =>{
+
+let date = new Date()
+
+let hours = date.getHours();
+let minutes= date.getMinutes();
+let seconds = date.getSeconds();
+
+if (hours < 10){
+hours=`0`+hours
+}
+if (minutes < 10){
+minutes=`0`+minutes
+}
+if (seconds < 10){
+seconds=`0`+seconds
+}
+document.getElementById(`clockBlock`).innerHTML=hours+`:`+minutes+`:`+seconds;
+}
+ setInterval(() => {
+    clock()
+ }, 1000);
+
